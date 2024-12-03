d99fc38911a99b12f5e411d2c493e6c52d41f14b
YeonV <trusted.yeon@gmail.com>
2024-12-03 01:01:13 +0100

    Release 0.1.1

--- src/renderer/src/store/useStore.ts
+++ src/renderer/src/store/useStore.ts
@@ -1,22 +1,26 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import create from 'zustand'
-import { devtools } from 'zustand/middleware'
-import { combine } from 'zustand/middleware'
+import { create } from 'zustand'
+import { combine, devtools, persist } from 'zustand/middleware'
 import { storeBears } from './storeBears'
 import { storeTours } from './storeTours'
 import { storeUI } from './storeUI'
 
 export const useStore = create(
   devtools(
-    combine(
+    persist(
+      combine(
+        {
+          hackedBy: 'Blade'
+        },
+        (set: any) => ({
+          ui: storeUI(set),
+          tours: storeTours(set),
+          example: storeBears(set)
+        })
+      ),
       {
-        hackedBy: 'Blade'
-      },
-      (set: any) => ({
-        ui: storeUI(set),
-        tours: storeTours(set),
-        example: storeBears(set)
-      })
+        name: 'vitron-storage'
+      }
     )
   )
 )
